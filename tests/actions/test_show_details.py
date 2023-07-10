f970be752def8a6d412c8fec390e429e8b36417a
Arthur Pelegrino <arthurpelegrino97@gmail.com>
2023-07-10 14:39:18 -0300

    requisito 4 corrigido

--- tests/actions/test_show_details.py
+++ tests/actions/test_show_details.py
@@ -1,17 +1,16 @@
 from pro_filer.actions.main_actions import show_details  # NOQA
+from datetime import date
 
-correct_result = (
-    "File name: projetos\n"
-    "File size in bytes: 4096\n"
-    "File type: directory\n"
-    "File extension: [no extension]\n"
-    "Last modified date: 2023-07-06"
-)
+# correct_result = (
+#     "File name: projetos\n"
+#     "File size in bytes: 4096\n"
+#     "File type: directory\n"
+#     "File extension: [no extension]\n"
+#     "Last modified date: 2023-07-06"
+# )
 
 
-def test_show_details(
-    capsys, correct_path_name, wrong_path_name, my_computer_patch
-):
+def test_show_details(capsys, correct_path_name, wrong_path_name, tmp_path):
     show_details(correct_path_name)
 
     captured = capsys.readouterr()
@@ -21,6 +20,23 @@ def test_show_details(
     captured = capsys.readouterr()
     assert captured.out.strip() == "File '????' does not exist"
 
-    show_details(my_computer_patch)
+    # show_details(my_computer_patch)
+    # captured = capsys.readouterr()
+    # assert captured.out.strip() == correct_result
+
+    file = tmp_path / "pro-filer-preview"
+    file.touch()
+
+    context = {"base_path": str(file)}
+
+    show_details(context)
+
     captured = capsys.readouterr()
-    assert captured.out.strip() == correct_result
+    expected_output = (
+        "File name: pro-filer-preview\n"
+        "File size in bytes: 0\n"
+        "File type: file\n"
+        "File extension: [no extension]\n"
+        f"Last modified date: {date.today()}\n"
+    )
+    assert captured.out == expected_output
